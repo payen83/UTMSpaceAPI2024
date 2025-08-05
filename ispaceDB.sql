0c8a1a8bad8ee5b9a3a71aa3a39f7aa674289b55
Ferdy Fauzi <payen83@gmail.com>
2025-08-05 18:31:51 +0800

    update sql

--- ispaceDB.sql
+++ ispaceDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Aug 05, 2025 at 07:11 AM
+-- Generation Time: Aug 05, 2025 at 10:30 AM
 -- Server version: 5.7.39
 -- PHP Version: 8.2.0
 
@@ -37,6 +37,15 @@ CREATE TABLE `news` (
   `date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`id`, `image_url`, `image_path`, `title`, `description`, `location`, `date`) VALUES
+(1, 'https://utmspace.edu.my/wp-content/uploads/2022/10/sembang-space.jpg', NULL, 'Explore endless education opportunities at our SPACE UTM Edu Fair!', 'UTMSPACE salah sebuah entiti di bawah naungan Universiti Teknologi Malaysia akan membuka pintu kepada umum bagi penganjuran SPACE UTM EDU FAIR 2024 pada bulan Jun.', 'johor bahru', '2024-05-27'),
+(10, 'https://utmspace.edu.my/wp-content/uploads/2022/10/stareducationfair.jpg', NULL, 'Star Education Fair 2022', 'Choose the right institution to secure your future at the most awaited education fair, Join us this 12 & 13 November at Tropicana Gardens Mall! Meet us at booth A33! Visit the link to register! ', 'Tropicana Gardens Mall', '2024-05-06'),
+(11, 'https://utmspace.edu.my/wp-content/uploads/2022/10/28oct1.jpg', NULL, 'Program Professional Certificate in Advances of Long Range Surveillance Radar', 'Program Professional Certificate in Advances of Long Range Surveillance Radar & Wireless Data Link telah pun berjalan pada 17 Oktober dan akan berlangsung sehingga 28 Oktober 2022 bertempat di UTM/UTMSPACE Johor Bahru dan TUDM Bukit Lunchu', 'TUDM Bukit Lunchu', '2024-05-22');
+
 -- --------------------------------------------------------
 
 --
@@ -65,6 +74,16 @@ CREATE TABLE `staff` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`id`, `token`, `image_url`, `email`, `password`, `first_name`, `last_name`, `ic`, `address`, `position`, `department`, `phone_no`, `birth_date`, `self_report_date`, `self_report_location`, `self_report_letter`, `self_report_document`, `tentative_program`) VALUES
+(1, '6e7f67b593261e2bbb327dc6670dcaef39a870774aada934a344584e0d70d049', NULL, 'test1@email.com', '$2y$10$Fxo9gjUVyG1mPwfpKctCKOMOqhHX1szepP8ZUDzVmYrm3K1P1elg6', 'test1', 'Tested1', NULL, NULL, 'tester1', 'Testing1', '1111111111', '1991-01-01', NULL, NULL, NULL, NULL, NULL),
+(4, NULL, NULL, 'test2@email.com', '$2y$10$xV6IDx1MqrAvZhswXVZ6J.aaIRdw458TLwSaKBvdISqq9B/Pan13W', 'test2', 'Tested2', NULL, NULL, 'tester2', 'Testing2', '2222222222', '1991-02-02', NULL, NULL, NULL, NULL, NULL),
+(5, NULL, NULL, 'test4@email.com', '$2y$10$bvAfCfIhjzg7tLDAPGABmO82SECRXROaKgO4m6aocMu3qU.uiz6di', 'staff5', 'Tested3', NULL, NULL, 'tester3', 'Testing3', '3333333333', '1991-03-03', NULL, NULL, NULL, NULL, NULL),
+(6, NULL, NULL, 'test5@email.com', '$2y$10$TY5uQkmCfJiMzwctKSUmku04TJw3pEgUmanv4gxKUG6AAn4DkmIfi', 'test5', 'Tested5', '900102-06-5456', 'No 999 Jalan Makmur', 'tester5', 'Testing5', '3333333333', '1991-03-03', '2024-01-01', 'Taman Bunga', '/files/2024926145434-i5fh7M.pdf', '/files/2024926145434-i5fh7M.pdf', '/files/2024926145434-i5fh7M.pdf');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -90,13 +109,13 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
